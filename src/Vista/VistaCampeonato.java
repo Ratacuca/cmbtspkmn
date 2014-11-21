8cbeeea335a180dd17b3f21da6786d99f553c966
Paulo <Paulo@Paulo-HP>
2014-11-21 20:43:30 -0300

    vista campeonato

--- src/Vista/VistaCampeonato.java
+++ src/Vista/VistaCampeonato.java
@@ -234,198 +234,73 @@ public class VistaCampeonato extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jL_participante1.setText("jLabel1");
         getContentPane().add(jL_participante1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 4, 60, -1));
-
-        jL_participante2.setText("jLabel3");
         getContentPane().add(jL_participante2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 36, 60, 10));
-
-        jL_participante3.setText("jLabel4");
         getContentPane().add(jL_participante3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 66, 60, -1));
-
-        jL_participante4.setText("jLabel5");
         getContentPane().add(jL_participante4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 96, 60, -1));
-
-        jL_participante5.setText("jLabel6");
         getContentPane().add(jL_participante5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 124, 60, -1));
-
-        jL_participante6.setText("jLabel7");
         getContentPane().add(jL_participante6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 156, 60, -1));
-
-        jL_participante7.setText("jLabel8");
         getContentPane().add(jL_participante7, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 186, 60, -1));
-
-        jL_participante8.setText("jLabel9");
         getContentPane().add(jL_participante8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 220, 60, 10));
-
-        jL_participante9.setText("jLabel10");
         getContentPane().add(jL_participante9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 250, 60, 10));
-
-        jL_participante10.setText("jLabel11");
         getContentPane().add(jL_participante10, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, 60, -1));
-
-        jL_participante11.setText("jLabel12");
         getContentPane().add(jL_participante11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 310, 60, -1));
-
-        jL_participante12.setText("jLabel13");
         getContentPane().add(jL_participante12, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 340, 60, -1));
-
-        jL_participante13.setText("jLabel14");
         getContentPane().add(jL_participante13, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, 60, -1));
-
-        jL_participante14.setText("jLabel15");
         getContentPane().add(jL_participante14, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 400, 60, -1));
-
-        jL_participante15.setText("jLabel16");
         getContentPane().add(jL_participante15, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 430, 60, -1));
-
-        jL_participante16.setText("jLabel17");
         getContentPane().add(jL_participante16, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 460, 60, -1));
-
-        jL_participante17.setText("jLabel18");
         getContentPane().add(jL_participante17, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 4, 60, -1));
-
-        jL_participante18.setText("jLabel19");
         getContentPane().add(jL_participante18, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 34, 60, -1));
-
-        jL_participante19.setText("jLabel20");
         getContentPane().add(jL_participante19, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 66, 60, -1));
-
-        jL_participante20.setText("jLabel21");
         getContentPane().add(jL_participante20, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 100, 60, 10));
-
-        jL_participante21.setText("jLabel22");
         getContentPane().add(jL_participante21, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 124, 60, -1));
-
-        jL_participante22.setText("jLabel23");
         getContentPane().add(jL_participante22, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 154, 60, -1));
-
-        jL_participante23.setText("jLabel24");
         getContentPane().add(jL_participante23, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 190, 60, 10));
-
-        jL_participante24.setText("jLabel25");
         getContentPane().add(jL_participante24, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 220, 60, 10));
-
-        jL_participante25.setText("jLabel26");
         getContentPane().add(jL_participante25, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 250, 60, 10));
-
-        jL_participante26.setText("jLabel27");
         getContentPane().add(jL_participante26, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 280, 60, -1));
-
-        jL_participante27.setText("jLabel28");
         getContentPane().add(jL_participante27, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 310, 60, -1));
-
-        jL_participante28.setText("jLabel29");
         getContentPane().add(jL_participante28, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 340, 60, -1));
-
-        jL_participante29.setText("jLabel30");
         getContentPane().add(jL_participante29, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 370, 60, -1));
-
-        jL_participante30.setText("jLabel31");
         getContentPane().add(jL_participante30, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 400, 60, -1));
-
-        jL_participante31.setText("jLabel32");
         getContentPane().add(jL_participante31, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 430, 60, -1));
-
-        jL_participante32.setText("jLabel33");
         getContentPane().add(jL_participante32, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 464, 60, 10));
-
-        jL_participante_1_1.setText("jLabel34");
         getContentPane().add(jL_participante_1_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 20, 60, -1));
-
-        jL_participante_1_2.setText("jLabel35");
         getContentPane().add(jL_participante_1_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 84, 60, 10));
-
-        jL_participante_1_3.setText("jLabel36");
         getContentPane().add(jL_participante_1_3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 60, -1));
-
-        jL_participante_1_4.setText("jLabel37");
         getContentPane().add(jL_participante_1_4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 204, 60, 10));
-
-        jL_participante_1_5.setText("jLabel38");
         getContentPane().add(jL_participante_1_5, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 266, 60, -1));
-
-        jL_participante_1_6.setText("jLabel39");
         getContentPane().add(jL_participante_1_6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 328, 60, 10));
-
-        jL_participante_1_7.setText("jLabel40");
         getContentPane().add(jL_participante_1_7, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 384, 60, -1));
-
-        jL_participante_1_8.setText("jLabel41");
         getContentPane().add(jL_participante_1_8, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 446, 60, -1));
-
-        jL_participante_1_9.setText("jLabel42");
         getContentPane().add(jL_participante_1_9, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 20, 60, -1));
-
-        jL_participante_1_10.setText("jLabel43");
         getContentPane().add(jL_participante_1_10, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 84, 60, 10));
-
-        jL_participante_1_11.setText("jLabel44");
         getContentPane().add(jL_participante_1_11, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 140, 60, -1));
-
-        jL_participante_1_12.setText("jLabel45");
         getContentPane().add(jL_participante_1_12, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 200, 60, -1));
-
-        jL_participante_1_13.setText("jLabel46");
         getContentPane().add(jL_participante_1_13, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 266, 60, 10));
-
-        jL_participante_1_14.setText("jLabel47");
         getContentPane().add(jL_participante_1_14, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 328, 60, 10));
-
-        jL_participante_1_15.setText("jLabel48");
         getContentPane().add(jL_participante_1_15, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 386, 60, 10));
-
-        jL_participante_1_16.setText("jLabel49");
         getContentPane().add(jL_participante_1_16, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 446, 60, -1));
-
-        jL_participante_2_1.setText("jLabel50");
         getContentPane().add(jL_participante_2_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 56, 70, 10));
-
-        jL_participante_2_2.setText("jLabel51");
         getContentPane().add(jL_participante_2_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 170, 60, 10));
-
-        jL_participante_2_3.setText("jLabel52");
         getContentPane().add(jL_participante_2_3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 300, 60, 10));
-
-        jL_participante_2_4.setText("jLabel53");
         getContentPane().add(jL_participante_2_4, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 414, 60, 10));
-
-        jL_participante_2_5.setText("jLabel54");
         getContentPane().add(jL_participante_2_5, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 54, 60, -1));
-
-        jL_participante_2_6.setText("jLabel55");
         getContentPane().add(jL_participante_2_6, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 170, 60, 10));
-
-        jL_participante_2_7.setText("jLabel56");
         getContentPane().add(jL_participante_2_7, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 300, 60, 10));
-
-        jL_participante_2_8.setText("jLabel57");
         getContentPane().add(jL_participante_2_8, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 414, 60, 10));
-
-        jL_participante_3_1.setText("jLabel58");
         getContentPane().add(jL_participante_3_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 110, 60, -1));
-
-        jL_participante_3_2.setText("jLabel59");
         getContentPane().add(jL_participante_3_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 354, 60, -1));
-
-        jL_participante_3_3.setText("jLabel60");
         getContentPane().add(jL_participante_3_3, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 110, 60, -1));
-
-        jL_participante_3_4.setText("jLabel61");
         getContentPane().add(jL_participante_3_4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 356, 60, 10));
 
         jL_participante_5_1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jL_participante_5_1.setText("jLabel62");
         getContentPane().add(jL_participante_5_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 40, 100, 30));
 
         jButton1.setText("Iniciar Combate");
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(343, 420, 110, -1));
-
-        jL_participante_4_1.setText("jLabel3");
         getContentPane().add(jL_participante_4_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 236, 60, -1));
-
-        jL_participante_4_2.setText("jLabel4");
         getContentPane().add(jL_participante_4_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 236, 60, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/campeonato1.png"))); // NOI18N
